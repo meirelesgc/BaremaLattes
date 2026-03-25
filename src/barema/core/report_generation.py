4ad0f8d85dc2436733249150d2038cb87068d37b
meirelesgc <geu_docsta@outlook.com>
2026-03-25 16:29:48 -0300

    nothing here

--- src/barema/core/report_generation.py
+++ src/barema/core/report_generation.py
@@ -34,162 +34,6 @@ from barema.services.report_utils import (
 current_year = datetime.now().year
 
 
-FILES = [
-    "data/csv/researcher_profile.csv",
-    "data/csv/technological_production_and_innovation.csv",
-    "data/csv/transfer_of_technology.csv",
-    "data/csv/participation_in_project.csv",
-    "data/csv/human_resources.csv",
-    "data/csv/project_analysis.csv",
-]
-
-CONFIG = [
-    {
-        "original": "last_update",
-        "new": "Ultima atualização",
-        "default": "Erro",
-    },
-    {
-        "original": "nome",
-        "new": "Nome",
-        "default": "Não informado",
-    },
-    {
-        "original": "lattes_id",
-        "new": "Identificador Lattes",
-        "default": "Não informado",
-    },
-    {"original": "_", "new": "Avaliador", "default": "Não informado"},
-    {"original": "_", "new": "Segundo olhar", "default": "Não informado"},
-    {"original": "_", "new": "Classificação", "default": "Não informado"},
-    {"original": "_", "new": "Projeto", "default": "Não informado"},
-    {"original": "_", "new": "Proponente", "default": "Não informado"},
-    {"original": "_", "new": "Súmula realizações", "default": "Não extraido"},
-    {
-        "original": "_",
-        "new": "Transferência tecnologia impacto",
-        "default": "Não extraido",
-    },
-    {"original": "_", "new": "Observação inicial", "default": "Não informado"},
-    {"original": "_", "new": "Extensão inovadora", "default": "Não informado"},
-    {"original": "_", "new": "Coeficiente súmula", "default": "Não informado"},
-    {"original": "_", "new": "Ad hocs", "default": "Não informado"},
-    {"original": "tempo_doutorado", "new": "Tempo doutorado", "default": 0},
-    {"original": "nivel_bolsa", "new": "Nível bolsista", "default": 0},
-    {"original": "h_index", "new": "Fator h", "default": 0},
-    {"original": "_", "new": "Maternidade adoção", "default": "Não extraido"},
-    {"original": "total_articles", "new": "Artigos periódicos", "default": 0},
-    {"original": "total_books", "new": "Livros capítulos", "default": 0},
-    {"original": "total_software", "new": "Software com registro", "default": 0},
-    {"original": "total_no_reg_software", "new": "Software sem registro", "default": 0},
-    {"original": "total_cultivar_patents", "new": "Patentes cultivares", "default": 0},
-    {
-        "original": "total_other_technical_production",
-        "new": "Outros produtos tecnológicos",
-        "default": 0,
-    },
-    {"original": "_", "new": "Soma produção tecnológica", "default": 0},
-    {"original": "_", "new": "Nota produção tecnológica", "default": 0},
-    {"original": "_", "new": "Nota coeficiente súmula produção", "default": 0},
-    {
-        "original": "licenciamento_qtd",
-        "new": "Licenciamento transferência",
-        "default": 0,
-    },
-    {"original": "servicos_qtd", "new": "Serviços tecnológicos", "default": 0},
-    {"original": "empresas_qtd", "new": "Empresas terceiro setor", "default": 0},
-    {"original": "demanda_qtd", "new": "Demanda", "default": 0},
-    {"original": "_", "new": "Carta", "default": 0},
-    {"original": "_", "new": "Soma transferência tecnologia", "default": 0},
-    {"original": "_", "new": "Nota transferência tecnologia", "default": 0},
-    {
-        "original": "total_scientific_projects",
-        "new": "Coord projetos científicos",
-        "default": 0,
-    },
-    {
-        "original": "total_projects_with_companies",
-        "new": "Coord projetos empresas",
-        "default": 0,
-    },
-    {
-        "original": "total_research_projects",
-        "new": "Coord projetos pesquisa",
-        "default": 0,
-    },
-    {"original": "_", "new": "Soma projetos", "default": 0},
-    {"original": "_", "new": "Nota projetos", "default": 0},
-    {"original": "_", "new": "Nota coeficiente súmula projetos", "default": 0},
-    {"original": "total_guidance_postdoc", "new": "Pós doutorado", "default": 0},
-    {"original": "total_phd_completed", "new": "Doutorado concluído", "default": 0},
-    {"original": "total_phd_ongoing", "new": "Doutorado andamento", "default": 0},
-    {"original": "total_msc_completed", "new": "Mestrado concluído", "default": 0},
-    {"original": "total_msc_ongoing", "new": "Mestrado andamento", "default": 0},
-    {"original": "_", "new": "Bolsas tecnológicas", "default": "Não extraido"},
-    {"original": "_", "new": "Bolsas ic outras", "default": "Não extraido"},
-    {
-        "original": "_",
-        "new": "Organização programas formação",
-        "default": "Não extraido",
-    },
-    {"original": "_", "new": "Capacitação rh", "default": "Não extraido"},
-    {"original": "_", "new": "Nota rh", "default": "Não extraido"},
-    {"original": "_", "new": "Nota coeficiente súmula rh", "default": "Não extraido"},
-    {
-        "original": "publico_produto",
-        "new": "Público alvo produto",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "objetivos_metas_relevancia",
-        "new": "Objetivos metas relevância",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "metodologia_gestao",
-        "new": "Metodologia gestão",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "colaboracoes_financiamento",
-        "new": "Colaborações financiamento",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "potencial_inovacao_empreendedorismo",
-        "new": "Potencial inovação",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "demandas_escalabilidade",
-        "new": "Demandas escalabilidade",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "maturidade_resultados",
-        "new": "Maturidade resultados",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "organizacao_parcerias_extensao",
-        "new": "Organização parcerias extensão",
-        "default": "Projeto não encontrado",
-    },
-    {
-        "original": "perfil_tecnologico",
-        "new": "Perfil tecnológico",
-        "default": "Projeto não encontrado",
-    },
-    {"original": "_", "new": "Usou formulário", "default": 0},
-    {"original": "_", "new": "Observação projeto", "default": 0},
-    {"original": "_", "new": "Nota foco desenvolvimento", "default": 0},
-    {"original": "_", "new": "Parecer final", "default": 0},
-    {"original": "_", "new": "Nota final com súmula", "default": 0},
-    {"original": "_", "new": "Nota final sem súmula", "default": 0},
-    {"original": "_", "new": "Observação final", "default": 0},
-]
-
-
 def researcher_profile_csv(base_year=current_year):
     researchers = get_researchers()
     phd_time = get_phd_time()
@@ -413,21 +257,43 @@ def participation_in_project_csv():
     researchers.write_excel("data/csv/participation_in_project.xlsx")
 
 
+def merge_all_reports():
+    files_to_merge = [
+        "data/csv/researcher_profile.csv",
+        "data/csv/technological_production_and_innovation.csv",
+        "data/csv/transfer_of_technology.csv",
+        "data/csv/project_analysis.csv",
+        "data/csv/human_resources.csv",
+        "data/csv/participation_in_project.csv",
+    ]
+
+    dfs = [pl.read_csv(f) for f in files_to_merge]
+    df_final = dfs[0]
+
+    for df in dfs[1:]:
+        overlapping_cols = [
+            col
+            for col in df.columns
+            if col in df_final.columns and col != "researcher_id"
+        ]
+        df_to_join = df.drop(overlapping_cols)
+        df_final = df_final.join(df_to_join, on="researcher_id", how="left")
+
+    df_final.write_csv("data/csv/output/unified_report.csv")
+    df_final.write_excel("data/csv/output/unified_report.xlsx")
+
+
 def generate_final_report():
     os.makedirs("data/csv/output", exist_ok=True)
 
-    print("researcher_profile_csv")
     researcher_profile_csv()
-    print("technological_production_and_innovation_csv")
     technological_production_and_innovation_csv()
-    print("transfer_of_technology_csv")
     transfer_of_technology_csv()
-    print("project_analysis_csv")
     project_analysis_csv()
-    print("human_resources_csv")
     human_resources_csv()
-    print("participation_in_project_csv")
     participation_in_project_csv()
+
+    merge_all_reports()
 
 
 if __name__ == "__main__":
